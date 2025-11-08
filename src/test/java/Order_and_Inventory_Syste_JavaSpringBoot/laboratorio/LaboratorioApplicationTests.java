ee5eab14db24b6e8d41abe72351581482a50825a
Marcelo Pereira <marcelo.pereira@solucx.com.br>
2025-11-08 01:56:58 -0300

    initial project

---
+++ src/test/java/Order_and_Inventory_Syste_JavaSpringBoot/laboratorio/LaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package Order_and_Inventory_Syste_JavaSpringBoot.laboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
